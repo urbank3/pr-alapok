2747df0bdf2d5a6857deb4dea5055febbba9c553
urbank3 <112930909+urbank3@users.noreply.github.com>
2022-10-04 09:40:57 +0200

    Add files via upload

---
+++ 9_10_ora.py
@@ -0,0 +1,24 @@
+# 9 DK tankönyv feladata
+"""
+IDEI_ÉV = 2022
+print('az idei év :', IDEI_ÉV, sep='--->')
+felhasználó_kora = input('hány éves vagy?')
+print(type(felhasználó_kora))
+print('Te most', felhasználó_kora, 'éves vagy.')
+felhasználó_kora = int(felhasználó_kora)
+születési_év = IDEI_ÉV - felhasználó_kora
+print('Ekkor születtél:', születési_év, '.', sep="")
+print(type(felhasználó_kora))"""
+
+
+# 2. program
+gondolt_szám = 4
+tipp = input('gondolzam wgy számra.    Tippeld meg!!!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('ügyes!')
+    print('Gratulálok')
+else:
+    print('Hosszan Gondolkodtál Rajta?:)')
+    print('Nem Érte Meg.;)')
+print('PÁPÁ')
