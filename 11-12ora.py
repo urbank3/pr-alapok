66e4131b5d29dad041567c20a63fdfbb3ae3c162
urbank3 <112930909+urbank3@users.noreply.github.com>
2022-10-11 09:39:29 +0200

    Add files via upload

---
+++ 11-12ora.py
@@ -0,0 +1,30 @@
+'''
+# 2. program
+gondolt_szám = 4
+tipp = input('gondolzam wgy számra.    Tippeld meg!!!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('ügyes!')
+    print('Gratulálok')
+else:
+    print('Hosszan Gondolkodtál Rajta?:)')
+    print('Nem Érte Meg.;)')
+print('PÁPÁ')'''
+
+# feladat3
+szam1 = input (int( "kerek egy szamot"))
+szam2 = input(int("kerek egy masik szamot"))
+
+if szam1 == szam2:
+    print("megegyezik")
+
+if szam1 != szam2:
+    print("nem egyezik meg")
+
+if szam1 < szam2:
+    print("1. szam kisebb ")
+
+else:
+    print("az 1. szam nagyobb vagy egyenlő")
+    
+          
