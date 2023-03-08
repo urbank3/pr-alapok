6e8b9114f6370367dc35c2484ba88fb3b1155f63
urbank3 <112930909+urbank3@users.noreply.github.com>
2023-03-08 07:50:36 +0100

    Add files via upload

---
+++ 03.08_doga_UrbanKeve.py
@@ -0,0 +1,15 @@
+# A Urbán Keve 03.08 
+
+
+
+szam_sorozat = [15, 4, 5, 19, 2]
+
+darab =  0
+
+
+for szam in szam_sorozat:
+    if szam * 0 == 0:
+        darab = darab + 1
+
+print("Ennyi szám van a listában",darab)        
+
