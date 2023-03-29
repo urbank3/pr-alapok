81de53684e0601264dc65339da02bf17e2ea58c2
urbank3 <112930909+urbank3@users.noreply.github.com>
2023-03-29 08:20:24 +0200

    Add files via upload

---
+++ fuggveny_01.py
@@ -0,0 +1,9 @@
+#ord()	A Python ord() függvény egyetlen Unicode karakterből álló string argumentumot vesz fel, és egész számot ad vissza a Unicode kódpont értékéből.
+#chr()  A chr()metódus egy egész számot unicode karakterré alakít, és visszaadja.
+
+
+teszt_lista = [0,1,2,3,4,5,6,7,8,9]
+  
+print(min(teszt_lista))
+print(max(teszt_lista))
+print(teszt_lista(2))
---
+++ fuggveny_02.py
@@ -0,0 +1,18 @@
+
+#lower()        This function creates a new string in lowercase.
+#upper()        We can convert a string to uppercase in Python using str.upper() function.
+#capitalize()   Python String capitalize() function returns the capitalized version of the string.
+#endswith()     Python string endswith() function returns True if the string ends with the given suffix, otherwise it returns False.
+#find()         Python String find() method is used to find the index of a substring in a string.
+#isalnum()      Python string isalnum() function returns True if it’s made of alphanumeric characters only.
+#isalpha()      Python String isalpha() function returns True if all the characters in the string are alphabets, otherwise False.
+#islower()      Python String islower() returns True if all cased characters in the string are lowercase and there is at least one cased character, otherwise it returns False.
+#join()         This function returns a new string that is the concatenation of the strings in iterable with string object as a delimiter.
+#replace()      Python string replace() function is used to create a new string by replacing some parts of another string.
+#rfind()        The rfind() method finds the last occurrence of the specified value.
+#rstrip()       The rstrip() method removes any trailing characters (characters at the end a string), space is the default trailing character to remove.
+#startswith()   Python string startswith() function returns True if the string starts with the given prefix, otherwise it returns False.
+#strip()        Used to trim whitespaces from the string object.
+#swapcase()     Python String swapcase() function returns a new string with uppercase characters converted to lowercase and vice versa.
+#title()        Python String title() function returns a title cased version of the string.
+#center()       The center() method will center align the string, using a specified character (space is default) as the fill character.
